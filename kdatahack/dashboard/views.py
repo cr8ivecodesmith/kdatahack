5d46e8cfa0abe6a74a4e24ee093ac6a250b03499
Ray Dino <ray.e.dino@gmail.com>
2014-11-22 18:26:11 +0800

    Created dashboard app

---
+++ kdatahack/dashboard/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+class HomePage(TemplateView):
+    pass
+
+
+class ResearchPage(TemplateView):
+    pass
+
+
+class ItemDetailPage(TemplateView):
+    pass
