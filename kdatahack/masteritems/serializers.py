97cc5282d0ab00c0d5e2b1390f3fe0ab2198a355
Matt Lebrun <talk@mattlebrun.com>
2014-11-23 07:24:02 +0800

    Added market_price to the masteritem serializer

--- kdatahack/masteritems/serializers.py
+++ kdatahack/masteritems/serializers.py
@@ -4,5 +4,13 @@ from .models import MasterItem
 
 
 class MasterItemSerializer(serializers.ModelSerializer):
+    market_price = serializers.Field(source='_get_market_price')
     class Meta:
         model = MasterItem
+        fields = (
+            'id',
+            'item_name',
+            'item_description',
+            'uom',
+            'market_price',
+        )
