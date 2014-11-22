8e1799b1fbca3e7cb69810c66fe52621799ed8c6
Ray Dino <ray.e.dino@gmail.com>
2014-11-23 00:50:54 +0800

    mgmt cmd fix

--- kdatahack/masteritems/management/commands/match_items.py
+++ kdatahack/masteritems/management/commands/match_items.py
@@ -1,23 +1,26 @@
 from django.core.management.base import BaseCommand
-from .models import MasterItem
+from masteritems.models import MasterItem
 from philgeps.models import Awards
 
 
 class Command(BaseCommand):
     
     def handle(self, *args, **options):
-        for award in Awards.objects.all().iterator():
+        for award in Awards.objects.filter(master_item__isnull=True).iterator():
             if MasterItem.objects.filter(
                 item_name__iexact=award.item_name, uom=award.uom).exists():
                 master_item = MasterItem.objects.get(item_name__iexact=award.items, uom=award.uom)
                 award.master_item = master_item
+                print "{} <- {}".format(master_item.item_name, award.item_name)
                 award.save()
             else:
                 master_item = MasterItem(
                     item_name=award.item_name,
                     item_description=award.item_description,
                     uom=award.uom)
+                print "Creating Master Item: {}".format(master_item.item_name)
                 master_item.save()
                 award.master_item = master_item
+                print "{} <- {}".format(master_item.item_name, award.item_name)
                 award.save()
                 
