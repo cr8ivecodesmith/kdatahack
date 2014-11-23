756d9333f36e344699845d5fb65a8de42929dfba
Ray Dino <ray.e.dino@gmail.com>
2014-11-23 08:06:37 +0800

    Report view

--- kdatahack/dashboard/views.py
+++ kdatahack/dashboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.http import Http404
+from django.views.generic.base import TemplateView, View
+from django.http import HttpResponse, Http404
+from itertools import izip
+import tablib
 
 from masteritems.models import MasterItem
 
@@ -24,3 +26,31 @@ class ItemDetailPage(TemplateView):
             raise Http404
         context['item'] = mitem
         return context
+
+
+class ReportPage(View):
+   
+    def get(self, request, *args, **kwargs):
+        print request
+        items = request.GET.getlist('items', [])
+        qtys = request.GET.getlist('qtys', [])
+        table = tablib.Dataset()
+        table.headers = ['Item Name', 'Item Description', 'Price', 'Quantity', 'UOM', 'Total']
+        grand_total = 0
+        for item, qty in izip(items, qtys):
+            master = MasterItem.objects.get(pk=item)
+            total = float(master.market_price) * float(qty)
+            table.append([
+                master.item_name,
+                master.item_description,
+                master.market_price,
+                qty,
+                master.uom,
+                total])
+            grand_total += total
+        table.append(['','','','','GRAND TOTAL', grand_total])
+        response = HttpResponse(content_type='text/plain')
+        response['Content-Disposition'] = 'attachment; filename="MarketResearch.csv"'
+        response.write(table.csv)
+        return response
+
