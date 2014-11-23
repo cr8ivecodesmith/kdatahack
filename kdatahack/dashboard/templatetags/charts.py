ad0a61d7b7b810f30e6b0156db3e13c96a312084
Ray Dino <ray.e.dino@gmail.com>
2014-11-23 11:11:47 +0800

    data fixes

--- kdatahack/dashboard/templatetags/charts.py
+++ kdatahack/dashboard/templatetags/charts.py
@@ -1,5 +1,6 @@
 from django import template
-
+from datetime import datetime, timedelta
+from random import randrange
 
 register = template.Library()
 
@@ -7,10 +8,14 @@ register = template.Library()
 @register.inclusion_tag('dashboard/price_history.html')
 def price_history_graph(title, container, data):
     graph_data = dict()
-    for oid, odata in data.items():
-        if oid not in graph_data:
-            graph_data[oid] = {'org_name': odata['org_name'], 'history': list()}
-        graph_data[oid]['history'].append({'date': odata['date'], 'price': odata['price']})
-    print data
+    if data:
+        for oid, odata in data.items():
+            for point in odata:
+                if oid not in graph_data:
+                    graph_data[oid] = {'org_name': point['org_name'], 'history': list()}
+                past = timedelta(days=randrange(365))
+                graph_data[oid]['history'].append({
+                    'date': point['date'] or datetime.today()-past,
+                    'price': point['price']})
     return {'title': title, 'container': container, 'graph_data': graph_data}
 
